0c702f1c8a4b29a30bcf582ce4b53b07d45f38f4
diogo <diogobecosilva15@gmail.com>
2024-11-30 17:14:23 +0000

    seeders
    created seeders for filling up the tables (still need to figure out how to get data from the api in order to insert it into the tables)

--- library_project/database/seeders/DatabaseSeeder.php
+++ library_project/database/seeders/DatabaseSeeder.php
@@ -15,9 +15,23 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    
+        $this->call(
+            [
+            CategorySeeder::class,
+            BookSeeder::class,
+            BookCategorySeeder::class,
+            RoleSeeder::class,
+            ReaderSeeder::class,
+            RequestSeeder::class,
+            WishlistSeeder::class,
+            BookWishlistSeeder::class
         ]);
     }
+
+    
 }
