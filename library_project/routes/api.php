fe461d07998aecfd572ed73ca0de372dd3918076
diogo <diogobecosilva15@gmail.com>
2024-12-18 17:15:35 +0000

    removed duplicate migration

--- library_project/routes/api.php
+++ library_project/routes/api.php
@@ -9,4 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-#Route::middleware('auth:sanctum')->get('/api/books', [ApiController::class, 'getApi']);
+Route::middleware('auth:sanctum')->get('/api/books', [ApiController::class, 'getApi']);
